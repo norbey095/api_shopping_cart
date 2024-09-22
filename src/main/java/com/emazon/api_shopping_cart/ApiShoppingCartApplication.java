4aaed6dc58d1072eae91ffcb3ff1019d7edd813a
norbey095 <norbey095@gmail.com>
2024-09-22 11:03:15 -0500

    Shopping cart base project

---
+++ src/main/java/com/emazon/api_shopping_cart/ApiShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.api_shopping_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiShoppingCartApplication.class, args);
+	}
+
+}
